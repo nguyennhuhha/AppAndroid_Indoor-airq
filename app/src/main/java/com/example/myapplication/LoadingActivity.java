5d0e54eb3fc556d43e0ad24de494f69dcc0c1da3
21522057 <21522057@gm.uit.edu.vn>
2023-11-01 23:27:11 +0700

    update

--- app/src/main/java/com/example/myapplication/LoadingActivity.java
+++ app/src/main/java/com/example/myapplication/LoadingActivity.java
@@ -2,9 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -39,7 +41,6 @@ import retrofit2.Response;
 public class LoadingActivity extends AppCompatActivity {
     private WebView webView;
     private ImageView img;
-    boolean isLoggedIn = false;
     public String token;
     @Override
     //quay trở về trang trước
@@ -52,14 +53,16 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
         webView = findViewById(R.id.web_login);
-        webView.getSettings().setJavaScriptEnabled(true);
         img = findViewById(R.id.back_btn);
         img.setOnClickListener(new View.OnClickListener() {//quay về trang trước
             @Override
             public void onClick(View v) {
-                onBackPressed();
+//                onBackPressed();
+                Intent it = new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(it);
             }
         });
+
         Intent intent = getIntent();
         //1: login, 2: signup
         int received = intent.getIntExtra("fragment", 0);
@@ -74,39 +77,111 @@ public class LoadingActivity extends AppCompatActivity {
             String pwd = intent.getStringExtra("signup_pass");
             String rePwd=intent.getStringExtra("signup_cfmpass");
 
-            String url ="https://uiot.ixxc.dev/auth/realms/master/protocol/openid-connect/auth?client_id=openremote&redirect_uri=https%3A%2F%2Fuiot.ixxc.dev%2Fmanager%2F&state=b2205ccc-4679-4c2b-97c5-bdfdf3567e37&response_mode=fragment&response_type=code&scope=openid&nonce=f75830f5-71af-4d8d-bf2e-19723e1548b2";
-            webView.loadUrl(url);
+            //String url ="https://uiot.ixxc.dev/auth/realms/master/protocol/openid-connect/auth?client_id=openremote&redirect_uri=https%3A%2F%2Fuiot.ixxc.dev%2Fmanager%2F&state=ec4fb068-fcb9-4247-870f-caceaf59bc0f&response_mode=fragment&response_type=code&scope=openid&nonce=e2762ea1-2f4f-4343-9738-ffa814e3274a/";
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setLoadWithOverviewMode(true);
+            webView.getSettings().setUseWideViewPort(true);
+            webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+            webView.getSettings().setDatabaseEnabled(false);
+            webView.getSettings().setDomStorageEnabled(true);
+            webView.getSettings().setGeolocationEnabled(false);
+            webView.getSettings().setSaveFormData(false);
+
+            String baseurl ="https://uiot.ixxc.dev";
+            webView.setWebViewClient(new WebViewClient());
             webView.setWebViewClient(new WebViewClient() {
                 @Override
                 public void onPageFinished(WebView view, String url) {
-                    // Khi trang web đã tải xong
-                    // Tìm và nhấn nút SIGN UP
                     //super.onPageFinished(view, url);
-                    webView.loadUrl("javascript:(function(){" +
-                            "var elements = document.getElementsByClassName('btn waves-effect waves-light');" +
-                            "for (var i = 0; i < elements.length; i++) {" +
-                            "    var element = elements[i];" +
-                            "    if (element.innerText.includes('SIGN UP')) {" +
-                            "        element.click();" + // Nhấn vào phần tử <a>
-                            "    }" +
-                            "}" +
-                            "})()");
-                    String usrScript = "document.getElementById('username').value='" + usr + "';";
-                    String emailScript = "document.getElementById('email').value='" + email + "';";
-                    String pwdScript = "document.getElementById('password').value='" + pwd + "';";
-                    String rePwdScript = "document.getElementById('password-confirm').value='" + rePwd + "';";
+                    // Tìm và nhấn vào nút sau khi trang đã tải xong
+                    if (url.contains(baseurl + "/auth/realms/master/protocol/openid-connect/auth?client_id=openremote")) {
+                        // Thực hiện click vào nút SIGN UP khi địa chỉ URL khớp
+                        webView.loadUrl("javascript:(function(){" +
+                                "document.getElementsByClassName('btn waves-effect waves-light')[1].click();" +
+                                "})()");
+                        webView.setWebViewClient(new WebViewClient(){
+                            @Override
+                            public void onPageFinished(WebView view, String url) {
+                                String usrScript = "document.getElementById('username').value='" + usr + "';";
+                                String emailScript = "document.getElementById('email').value='" + email + "';";
+                                String pwdScript = "document.getElementById('password').value='" + pwd + "';";
+                                String rePwdScript = "document.getElementById('password-confirm').value='" + rePwd + "';";
 
-                    view.evaluateJavascript(usrScript, null);
-                    view.evaluateJavascript(emailScript, null);
-                    view.evaluateJavascript(pwdScript, null);
-                    view.evaluateJavascript(rePwdScript, null);
-                    view.evaluateJavascript("document.getElementById('kc-register-form').submit();", null);
-                    CallLoginService(usr, pwd);
+                                view.evaluateJavascript(usrScript, null);
+                                view.evaluateJavascript(emailScript, null);
+                                view.evaluateJavascript(pwdScript, null);
+                                view.evaluateJavascript(rePwdScript, null);
+                                view.evaluateJavascript("document.getElementById('kc-register-form').submit();", null);
+
+                                CallSignUpService(usr, pwd);
+                            }
+                        });
+
+                    }
+                }
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    // Xử lý chuyển hướng sau khi nhấn nút
+                    if (url.startsWith(baseurl)) {
+                        view.loadUrl(url);
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            });
+            webView.loadUrl(baseurl);
+        }
+
+    }
+    private void CallLoginService(String name, String pass){
+        ProgressBar progressBar = findViewById(R.id.progress);
+        progressBar.setVisibility(View.VISIBLE);
+        try{
+            final String id = name;
+            final String key = pass;
+            APIInterface service = APIClient.getClient().create(APIInterface.class);
+            Call<ResponseBody> srvLogin = service.getToken("openremote",id,key,"password");
+            srvLogin.enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    if (response.isSuccessful()){
+                        try {
+                            String ResponseJson = response.body().string();
+                            Gson objGson = new Gson();
+                            tokenResponse objResp=objGson.fromJson(ResponseJson, tokenResponse.class);
+                            token = objResp.getAccess_token();
+                            Toast.makeText(LoadingActivity.this, token, Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoadingActivity.this, "Token success", Toast.LENGTH_SHORT).show();
+                            TextView text = findViewById(R.id.text_wait);
+                            text.setText("Success");
+                            progressBar.setVisibility(View.GONE);
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                            Toast.makeText(LoadingActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(LoadingActivity.this, "Invalid, Try again", Toast.LENGTH_SHORT).show();
+                        TextView text = findViewById(R.id.text_wait);
+                        text.setText("Login Failed \n Please try again!");
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    Toast.makeText(LoadingActivity.this, "System error", Toast.LENGTH_SHORT).show();
+                    Log.e("NetworkError", "Error: " + t.getMessage());
                 }
             });
         }
+        catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(LoadingActivity.this, "System error", Toast.LENGTH_SHORT).show();
+        }
     }
-    private void CallLoginService(String name, String pass){
+    private void CallSignUpService(String name, String pass){
         ProgressBar progressBar = findViewById(R.id.progress);
         progressBar.setVisibility(View.VISIBLE);
         try{
